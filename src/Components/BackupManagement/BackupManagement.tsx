e7c4fe24e06fd43bab131fc8a90d4de1b1ef3ea8
tcalik <tcalik@protonmail.com>
2023-07-24 15:23:04 +0200

    Import backup functionality added

--- src/Components/BackupManagement/BackupManagement.tsx
+++ src/Components/BackupManagement/BackupManagement.tsx
@@ -1,17 +1,25 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./BackupManagement.css";
 import copyToClipboard from "../../utils/copyToClipboard";
 
 const BackupManagement = () => {
   const [backupAreaVisible, setBackupAreaVisible] = useState(false);
+  const [backupMode, setBackupMode] = useState<"save" | "import">("save");
+  const importContentRef = useRef<HTMLTextAreaElement | null>(null);
 
   const toggleBackupArea = () => {
     setBackupAreaVisible(!backupAreaVisible);
   };
 
   const getBackup = () => {
+    setBackupMode("save");
     toggleBackupArea();
     console.log(localStorage.getItem("notes"));
+  };
+
+  const importBackup = () => {
+    setBackupMode("import");
+    toggleBackupArea();
   };
   const getStorage = (): string => {
     if (localStorage.getItem("notes") !== null) {
@@ -23,9 +31,15 @@ const BackupManagement = () => {
     copyToClipboard(getStorage());
   };
 
+  const importToApp = () => {
+    if (importContentRef.current?.value)
+      localStorage.setItem("notes", importContentRef.current?.value);
+      else (console.error("Eerr"))
+  };
+
   return (
     <div className="BackupManagement">
-      {backupAreaVisible ? (
+      {backupAreaVisible && backupMode === "save" ? (
         <div>
           <p>Copy and save this text to use as a backup later</p>
           <textarea
@@ -45,10 +59,29 @@ const BackupManagement = () => {
             </button>
           </div>
         </div>
+      ) : backupAreaVisible && backupMode === "import" ? (
+        <div>
+          <p>Paste your backup and import</p>
+          <textarea ref={importContentRef} className="BackupText"></textarea>
+          <div className="BackupButtonsZone">
+            <button onClick={importToApp} className="BackupButton">
+              Import
+            </button>
+            <button
+              onClick={toggleBackupArea}
+              className="BackupButton CloseBackupButton"
+            >
+              Close
+            </button>
+          </div>
+        </div>
       ) : (
         <div className="BackupButtonsZone">
           <button className="BackupButton" onClick={getBackup}>
             Save Backup
+          </button>
+          <button className="BackupButton" onClick={importBackup}>
+            Import Backup
           </button>
         </div>
       )}
